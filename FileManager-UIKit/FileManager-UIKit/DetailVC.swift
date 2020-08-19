0d099585cc2b006957de0fdcca18e33c46e98ef6
Alejandro Terrazas <alex.terrazas.z@gmail.com>
2020-08-18 20:08:39 -0500

    Add Activity View Controller

--- FileManager-UIKit/FileManager-UIKit/DetailVC.swift
+++ FileManager-UIKit/FileManager-UIKit/DetailVC.swift
@@ -16,7 +16,7 @@ class DetailVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationItem.largeTitleDisplayMode = .never
+        configureNavigation()
         configureImage()
     }
     
@@ -58,6 +58,23 @@ class DetailVC: UIViewController {
         ])
         
         imageView.contentMode = .scaleAspectFit
+    }
+    
+    func configureNavigation() {
+        navigationItem.largeTitleDisplayMode = .never
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+    }
+    
+    @objc func shareTapped() {
+        
+        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+            print("No image found")
+            return
+        }
+        
+        let vc = UIActivityViewController(activityItems: [image], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
 
